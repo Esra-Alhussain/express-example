3b9fe613d57a0b991981c57e1102ebc287ea4768
Esra-Alhussain <esra.alhussain.s@gmail.com>
2024-01-15 22:06:28 -0800

    load data file on the server

--- express-app/index.js
+++ express-app/index.js
@@ -4,6 +4,9 @@ import data from './data/data.json'
 const app = express();  //create a variable tat will hold copy of express
 const PORT = 3000;
 
-app.listen(PORT, () =>   //listen to the port that we just created 
-   console.log(`Yourr server is runing on port ${PORT}`)
-);
+app.listen(PORT, () =>  {    //listen to the port that we just created 
+   console.log(`Yourr server is runing on port ${PORT}`);
+
+   //load data file on the server
+   console.log(data);
+});
